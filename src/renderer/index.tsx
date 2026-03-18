38f3669464d312979940f8d15aab48ff75e973ec
Dean Kruger <dean.kruger@gmail.com>
2026-03-18 08:51:30 +0000

    Update index.tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-if (!container){
+if (!container) {
     throw new Error('Root element not found');    
 }
 
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
-)
+);
