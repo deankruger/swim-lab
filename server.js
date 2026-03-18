f7022b5902ef6cc5d068cab27e675ab5a331d563
Dean Kruger <dean.kruger@gmail.com>
2026-03-18 10:44:50 +0000

    Update server.js Attempt CORS

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-// Force HTTPS on Azure (X-Forwarded-Proto is set by Azure's front-end)
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    return res.redirect(301, `https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
+//// Force HTTPS on Azure (X-Forwarded-Proto is set by Azure's front-end)
+//app.use((req, res, next) => {
+//  if (req.headers['x-forwarded-proto'] === 'http') {
+//    return res.redirect(301, `https://${req.headers.host}${req.url}`);
+//  }
+//  next();
+//});
 
-//const cors = require('cors');
-//app.use(cors({ origin: 'http://swim-lab.azurewebsites.net' }));
+const cors = require('cors');
+app.use(cors({ origin: 'http://swim-lab.azurewebsites.net' }));
 
 // API routes (scraping services - must be before static file serving)
 const apiRouter = require('./dist-server/server/api').default;
