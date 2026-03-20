2792ad64de08572fd21ad807a7def755997fd8a9
Dean Kruger <dean.kruger@gmail.com>
2026-03-20 12:00:09 +0000

    Update dataStore.ts

--- src/services/dataStore.ts
+++ src/services/dataStore.ts
@@ -1,10 +1,6 @@
-import { SwimmerData, CountyTimes, CountyTimesStore, ComparisonResult } from '../types';
+import { SwimmerData, CountyTimesStore } from '../types';
 import { FileStore } from './storage/FileStore';
 import { SwimmerRepository } from './repositories/SwimmerRepository';
-import { ExcelImporter } from './excel/ExcelImporter';
-import { ExcelExporter } from './excel/ExcelExporter';
-import { PdfImporter } from './pdf/PdfImporter';
-import { TimeConverter } from './utils/TimeConverter';
 
 interface StoredData {
     swimmers: SwimmerData[];
@@ -12,22 +8,10 @@ interface StoredData {
 
 class DataStore {
     private swimmerRepository: SwimmerRepository;
-    private excelExporter: ExcelExporter;
-    private excelImporter: ExcelImporter;
-    private pdfImporter: PdfImporter;
-
+    
     constructor() {
-        //Init file storefor swimmers
         const fileStore = FileStore.createInUserData<StoredData>('swimmers.json');
-
-        //Init Services
         this.swimmerRepository = new SwimmerRepository(fileStore);
-        this.excelExporter = new ExcelExporter();
-        const timeConverter = new TimeConverter();
-        this.excelImporter = new ExcelImporter(timeConverter);
-        this.pdfImporter = new PdfImporter();
-
-        //Init repository
         this.swimmerRepository.initialize().catch(err => {
             console.error('Failed to initialize swimmer repository:', err);
         });
@@ -47,30 +31,6 @@ class DataStore {
 
     async deleteSwimmer(tiref: string): Promise<boolean> {
         return this.swimmerRepository.deleteByTiref(tiref);
-    }
-
-    /**
-     *Exporting
-     */
-    async exportToExcel(swimmerData: SwimmerData, comparisonResult: ComparisonResult | null): Promise<string> {
-        return this.excelExporter.export(swimmerData, comparisonResult);
-    }
-
-    async importCountyTimesFromBase64(base64: string): Promise<CountyTimes> {
-        return await this.excelImporter.importCountyTimesFromBase64(base64);
-    }
-
-    async importCountyTimesFromBuffer(buffer: Uint8Array): Promise<CountyTimes> {
-        return await this.excelImporter.importCountyTimesFromBuffer(buffer);
-    }
-
-    async importFromBase64(base64: string, fileName: string): Promise<{ countyName: string, times: CountyTimes }> {
-        if (fileName.toLowerCase().endsWith('.pdf')) {
-            return await this.pdfImporter.importCountyTimesFromBase64(base64, fileName);
-        }
-        const times = await this.excelImporter.importCountyTimesFromBase64(base64);
-        const countyName = fileName.replace(/\.[^/.]+$/, '') || 'Unknown'; // Remove file extension
-        return { countyName, times };
     }
 
     async saveCountyTimesStore(store: CountyTimesStore): Promise<void> {
