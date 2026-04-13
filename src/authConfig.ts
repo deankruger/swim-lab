fc4e11eccba001b250b154967131f8555143355f
Dean Kruger <dean.kruger@gmail.com>
2026-04-13 07:13:44 +0100

    Update authConfig.ts added clientid and tenantid

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -2,8 +2,8 @@ import { Configuration, LogLevel } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: "your-client-id",
-        authority: "https://login.microsoftonline.com/your-tenant-id",
+        clientId: "2c93d326-0c47-4357-93fa-11063a6cb39a",
+        authority: "https://login.microsoftonline.com/0298244e-7f2d-46f7-b010-09fa360e73d0",
         redirectUri: "/", // or your deployed URL
      },
     cache: {
