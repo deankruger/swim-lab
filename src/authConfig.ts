70c83e8a438edf4d6fba280152085f02b823395d
Dean Kruger <dean.kruger@gmail.com>
2026-05-06 18:27:54 +0100

    Initial commit with some stub files

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,14 +1,20 @@
 import { Configuration, LogLevel } from "@azure/msal-browser";
 
+const EXTERNAL_ID_TENANT_SUBDOMAIN = "replace with tenant subdomain";
+const EXTERNAL_ID_TENANT_ID = "replace with tenant ID";
+const EXTERNAL_ID_CLIENT_ID = "replace with client ID";
+
+const ciamAuthorityHost = `${EXTERNAL_ID_TENANT_SUBDOMAIN}.ciamlogin.com`;
+
 export const msalConfig: Configuration = {
     auth: {
         clientId: "2c93d326-0c47-4357-93fa-11063a6cb39a",
-        authority: "https://login.microsoftonline.com/0298244e-7f2d-46f7-b010-09fa360e73d0",
-        redirectUri: "/", // or your deployed URL
+        authority: `https://${ciamAuthorityHost}/${EXTERNAL_ID_TENANT_ID}`,
+        knownAuthorities: [ciamAuthorityHost],
+        redirectUri: "/",
      },
     cache: {
-        cacheLocation: "localStorage",
-        // storeAuthStateInCookie: false,
+        cacheLocation: "localStorage"
     },
     system: {
         loggerOptions: {
@@ -18,5 +24,8 @@ export const msalConfig: Configuration = {
 };
 
 export const loginRequest = {
-    scopes: ["User.Read"], // or your API scopes.
+    scopes: ["openid", "profile", "offline_access"],
 };
+export const apiRequest = {
+    scopes: [`api://${EXTERNAL_ID_CLIENT_ID}/access_as_user`],
+}
